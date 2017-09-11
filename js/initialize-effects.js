0d179134930bb7f3af7bd19a0abee2e80cf2bf24
Dmitriy Beglov <ditransler@gmail.com>
2017-09-11 22:10:43 +0400

    Combined form-effects.js and initialize-effects.js

--- js/initialize-effects.js
+++ js/initialize-effects.js
@@ -1,13 +1,135 @@
 'use strict';
 
 (function () {
-  window.initializeEffects = function (effectsContainer, effectElem, applyEffect) {
-    effectsContainer.addEventListener('change', function onEffectControlsChange(evt) {
+  var EFFECTS = {
+    chrome: {
+      filter: 'grayscale',
+      max: 1
+    },
+    sepia: {
+      filter: 'sepia',
+      max: 1
+    },
+    marvin: {
+      filter: 'invert',
+      max: 100,
+      units: '%'
+    },
+    phobos: {
+      filter: 'blur',
+      max: 3,
+      units: 'px'
+    },
+    heat: {
+      filter: 'brightness',
+      max: 3
+    }
+  };
+
+  var currentEffect = 'none';
+
+  var form = document.querySelector('#upload-select-image');
+
+  var imagePreview = form.querySelector('.effect-image-preview');
+
+  var effectControls = form.querySelector('.upload-effect-controls');
+
+  var effectLevel = effectControls.querySelector('.upload-effect-level');
+  var effectLevelLine = effectControls.querySelector('.upload-effect-level-line');
+  var effectLevelPin = effectLevelLine.querySelector('.upload-effect-level-pin');
+  var effectLevelVal = effectLevelLine.querySelector('.upload-effect-level-val');
+
+  function getCoords(elem) {
+    var box = elem.getBoundingClientRect();
+
+    return {
+      left: box.left + window.pageXOffset,
+      top: box.top + window.pageYOffset
+    };
+  }
+
+  function updateEffectLevel(percents) {
+    var effect = EFFECTS[currentEffect];
+    var value = (typeof percents !== 'undefined') ? (effect.max * percents) / 100 : effect.max;
+
+    value = effect.units ? value + effect.units : value;
+
+    imagePreview.style.filter = effect.filter + '(' + value + ')';
+  }
+
+  function changeLevelDisplay() {
+    if (currentEffect === 'none') {
+      effectLevel.style.display = 'none';
+      imagePreview.style.filter = '';
+      return;
+    }
+
+    updateEffectLevel();
+    effectLevelPin.style.left = '100%';
+    effectLevelVal.style.width = '100%';
+    effectLevel.style.display = 'block';
+  }
+
+  changeLevelDisplay();
+
+  effectLevelPin.addEventListener('mousedown', function onEffectLevelPinMouseDown(evt) {
+    evt.preventDefault();
+
+    var pinCoords = getCoords(effectLevelPin);
+    var lineCoords = getCoords(effectLevelLine);
+    var shiftX = evt.pageX - pinCoords.left;
+
+    function onEffectLevelPinMouseMove(moveEvt) {
+      moveEvt.preventDefault();
+
+      var newLeft = moveEvt.pageX - shiftX - lineCoords.left;
+
+      if (newLeft < 0) {
+        newLeft = 0;
+      }
+
+      var rightEdge = effectLevelLine.offsetWidth;
+
+      if (newLeft > rightEdge) {
+        newLeft = rightEdge;
+      }
+
+      var percents = (newLeft * 100) / rightEdge;
+
+      updateEffectLevel(percents);
+
+      effectLevelPin.style.left = newLeft + 'px';
+      effectLevelVal.style.width = newLeft + 'px';
+    }
+
+    function onEffectLevelPinMouseUp(upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onEffectLevelPinMouseMove);
+      document.removeEventListener('mouseup', onEffectLevelPinMouseUp);
+    }
+
+    document.addEventListener('mousemove', onEffectLevelPinMouseMove);
+    document.addEventListener('mouseup', onEffectLevelPinMouseUp);
+
+    return false; // disable selection start (cursor change)
+  });
+
+  effectLevelPin.addEventListener('dragstart', function onEffectLevelPinDragStart(evt) {
+    evt.preventDefault();
+  });
+
+  window.initializeEffects = function (effectElem, applyEffect) {
+    effectControls.addEventListener('change', function onEffectControlsChange(evt) {
       if (evt.target.name !== 'effect') {
         return;
       }
 
-      applyEffect(effectElem, evt.target.value, effectElem.dataset.effect);
+      applyEffect(effectElem, evt.target.value, currentEffect);
+
+      currentEffect = evt.target.value;
+
+      changeLevelDisplay();
     });
   };
 })();
